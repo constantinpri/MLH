95ffec06025340c8144e2f864d0f592b62baad0f
constantinpri <pricochi.constantin@gmail.com>
2020-11-03 20:37:32 -0800

    "Created new test cases" "Added new files"

--- test/smoke/myTest.js
+++ test/smoke/myTest.js
@@ -1,5 +1,6 @@
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
+const data = require('../../data/testdata.json');
 
 describe('My Little Hero', function () { //define suite title by passing a string
 
@@ -10,22 +11,24 @@ describe('My Little Hero', function () { //define suite title by passing a strin
         });
 
         it('TC-002 ', function () {
-            const name = $(sel.name).setValue('Eric');
-            $$(sel.gender)[0].click();
-            $(sel.age).addValue(345);
-            $(sel.story).click();
-            $$(sel.type)[3].click();
-            //browser.pause(9000);
-            $(sel.creatBtn2).click();
-            //browser.pause(9000);
+            const name = $(sel.name).setValue(data.name);
+            const inputGender = $$(sel.gender)[data.gender.he].click();
+            const inputAge = $(sel.age).addValue(data.age);
+            const story = $(sel.story).click();
+            browser.pause(2000);
+            const type = $$(sel.type)[data.storyType].click();
+            const create = $(sel.creatBtn2).isEnabled();
+            const click = $(sel.creatBtn2).click();
             const end = $(sel.endStory).isDisplayed();
             expect(end).toEqual(true);
-            //const strory = $(sel.finalStory).getText();
-            //expect(strory).toHaveTextContaining("345");
-            //const def = $(sel.defaultp).isDisplayed();
-            //expect(def).toEqual(true);
+
+
         });
 
+        //const strory = $(sel.finalStory).getText();
+        //expect(strory).toHaveTextContaining("345");
+        //const def = $(sel.defaultp).isDisplayed();
+        //expect(def).toEqual(true);
 
 
     });
